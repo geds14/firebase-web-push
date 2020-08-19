07cbcb28f54296f4d44d454861733b8c99b2339a
Gil <gsamonte@citronsolutions.com>
2020-08-19 17:21:31 +0800

    fix3

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,5 +17,13 @@ firebase.initializeApp(firebaseConfig);
 const messaging=firebase.messaging();	
 
 messaging.setBackgroundMessageHandler(function(payload){
-	console.log(payload);
+    console.log(payload);
+    
+    const notification=JSON.parse(payload);
+    const notificationMessage={
+        body:notification.body,
+        title:notification.title
+    };
+    return self.registration.showNotification(payload.notification.title, notificationMessage);
+
 });		  
